5491f79e34d00d0419f1442d59b7c32a0fa29c7b
hyunjin9603 <hyunjin9603@gmail.com>
2023-11-02 15:51:18 +0900

    main 생성

---
+++ src/main/java/com/example/SpringVersion/SpringVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVersionApplication.class, args);
+	}
+
+}
